a1d63f58c7604307da02613aac22232db8ae8da7
hanifwidiyanto <hanifwidiyanto.12@gmail.com>
2023-04-17 10:27:56 +0700

    change filename visitor to graph

---
+++ src/pages/graph.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Graph() {
+  return (
+    <div className="w-full bg-white rounded-md p-8">
+      <h2 className="font-mulish text-lg">Total Visitors</h2>
+      <div className="mt-4">GRAPH</div>
+    </div>
+  );
+}
+
+export default Graph;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import Sidebar from './sidebar'
 import Navbar from './navbar'
-import Visitor from './visitor'
+import Graph from './graph'
 
 export default function Home() {
   return (
     <main className='bg-gray-200 h-screen w-screen px-64 pt-12'>
-       <Visitor />
+       <Graph />
     </main>
   )
 }
